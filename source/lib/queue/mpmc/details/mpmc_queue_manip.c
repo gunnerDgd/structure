3a7a62e21dc4f4168b19c2f915a0b793d1990780
gunnerDgd <ifstudiomail@naver.com>
2022-08-02 06:28:31 +0900

    MPMC Queue Updated.

--- source/lib/queue/mpmc/details/mpmc_queue_manip.c
+++ source/lib/queue/mpmc/details/mpmc_queue_manip.c
@@ -6,43 +6,35 @@ void*
         (__synapse_structure_mpmc_queue* pMpmcQueue)
 {
     __synapse_structure_mpmc_queue_node*
-        ptr_read;
+        ptr_read      = 0;
     void*
-        ptr_read_data;
-
+        ptr_read_data = 0;
+    
     do
     {
-        // Retrieves Read Pointer of MPMC Circular Queue
-        ptr_read 
+        ptr_read
             = pMpmcQueue->ptr_mpmc_rdptr;
-        // Retrieves Read Data from Read Pointer. This Might be NULL or Actual Data
-        ptr_read_data
-            = ptr_read->ptr_mpmc_data; 
         
-        // If There's no data to read (read_pointer == write_pointer),
-        // routine exits with return value 0.
-        if(ptr_read_data == NULL
-                || ptr_read == pMpmcQueue->ptr_mpmc_wrptr)
-                        return 0;
-    }
-    while
-        (!InterlockedCompareExchange64
-            (&ptr_read->ptr_mpmc_data, 0, ptr_read_data));
-    /*CAS with Read Pointer's Data.
-     * Only one thread can success this operation.
-     *
-     * Read Data == 0 -> The Read Pointer is already read by other thread.
-     * Read Data != 0 -> Read - Available State.
-     * 
-     * CAS -> Change Read Pointer's Data Pointer to NULL,
-     * To Specify other thread cannot read data from this pointer.
-     * 
-     * If ptr_read_data == 0, cmpxchg intrinsic (InterlockedCompareExchange) will return NULL,
-     */ 
+        if(ptr_read 
+                == pMpmcQueue->ptr_mpmc_wrptr)
+                    return 0;
+    } while
+        (InterlockedCompareExchange64
+            (&pMpmcQueue->ptr_mpmc_rdptr,
+                ptr_read->ptr_mpmc_next, ptr_read) != ptr_read);
     
-    InterlockedExchange64
-        (&pMpmcQueue->ptr_mpmc_rdptr,
-                ptr_read->ptr_mpmc_next);
+    do
+    {
+__do_mpmc_read:
+        ptr_read_data
+            = ptr_read->ptr_mpmc_data;
+        if(!ptr_read_data)
+            goto __do_mpmc_read;
+    } while
+        (InterlockedCompareExchange64
+            (&ptr_read->ptr_mpmc_data, 0, ptr_read_data) 
+                != ptr_read_data);
+
     return
         ptr_read_data;
 }
@@ -60,7 +52,6 @@ void*
                     (pMpmcQueue)));
     
     return ptr_read;
-
 }
 
 bool
@@ -68,23 +59,24 @@ bool
         (__synapse_structure_mpmc_queue* pMpmcQueue, void* pMpmcData)
 {
     __synapse_structure_mpmc_queue_node*
-        ptr_write;
+        ptr_write = 0;
 
     do
     {
-        ptr_write 
+        ptr_write
             = pMpmcQueue->ptr_mpmc_wrptr;
+        
         if(ptr_write->ptr_mpmc_next
                 == pMpmcQueue->ptr_mpmc_rdptr)
-                        return false;
-    }
+                    return false;
+    } while
+        (InterlockedCompareExchange64
+            (&pMpmcQueue->ptr_mpmc_wrptr, 
+                    ptr_write->ptr_mpmc_next, ptr_write) != ptr_write);
+    
     while
         (InterlockedCompareExchange64
-                (&ptr_write->ptr_mpmc_data, pMpmcData, 0));
-    
-    InterlockedExchange64
-        (&pMpmcQueue->ptr_mpmc_wrptr,
-                ptr_write->ptr_mpmc_next);
+            (&ptr_write->ptr_mpmc_data, pMpmcData, 0));
 
     return true;
 }
